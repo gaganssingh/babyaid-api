35488e177d841b92ca68f21e4b4bb680edb612d1
Gagan Singh <gagansingh.dev@outlook.com>
2020-02-04 15:22:55 -0500

    changed cors config

--- src/app.js
+++ src/app.js
@@ -16,11 +16,12 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(
-	cors({
-		origin : CLIENT_ORIGIN
-	})
-);
+app.use(cors());
+// app.use(
+// 	cors({
+// 		origin : CLIENT_ORIGIN
+// 	})
+// );
 
 app.use("/api/fourtosix", fourtosixRouter);
 app.use("/api/sixtoeight", sixtoeightRouter);
